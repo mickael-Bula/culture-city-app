8ca17f1fb0af9728984302858907f0ac63ac2ece
mickael-Bula <bula.mickael@gmail.com>
2022-02-25 11:07:07 +0100

    add/main controller

---
+++ src/Controller/Front/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/")
+     * @Route("/home", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('front/main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
