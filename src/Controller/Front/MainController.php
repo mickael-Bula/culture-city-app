0fad0a9349bce81b24663bef760024ca5ff34c45
mickael-Bula <bula.mickael@gmail.com>
2022-03-01 15:07:59 +0100

    delete/users displayed on front home for debug

--- src/Controller/Front/MainController.php
+++ src/Controller/Front/MainController.php
@@ -3,7 +3,7 @@
 namespace App\Controller\Front;
 
 
-use App\Repository\{ CategoryRepository, EventRepository, UserRepository };
+use App\Repository\{ CategoryRepository, EventRepository };
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,11 +14,10 @@ class MainController extends AbstractController
      * @Route("/")
      * @Route("/home", name="home")
      */
-    public function index(CategoryRepository $categoryRepository, EventRepository $eventRepository, UserRepository $userRepository): Response
+    public function index(CategoryRepository $categoryRepository, EventRepository $eventRepository): Response
     {
         $categories = $categoryRepository->findAll();
         $events = $eventRepository->findAll();
-        $users = $userRepository->findAll();
-        return $this->render('front/main/home.html.twig', compact('events', 'categories','users'));
+        return $this->render('front/main/home.html.twig', compact('events', 'categories'));
     }
 }
