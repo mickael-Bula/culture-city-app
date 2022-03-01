b183ea5c0ab772930f648cedfe634078cab8ee30
benoit-boulangier <benoit.boulangier@gmail.com>
2022-03-01 13:45:33 +0100

    fix/category-page

--- src/Controller/Front/CategoryController.php
+++ src/Controller/Front/CategoryController.php
@@ -2,8 +2,7 @@
 
 namespace App\Controller\Front;
 
-use App\Repository\CategoryRepository;
-use App\Repository\EventRepository;
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,24 +12,10 @@ class CategoryController extends AbstractController
     /**
      * @Route("/category/{slug}", name="category", methods={"GET"})
      */
-    public function showCategorySlug(CategoryRepository $categoryRepository, EventRepository $eventRepository, string $slug): Response
+    public function showByName(Category $category): Response
     {
-        $category = $categoryRepository->findOneBy(["slug"=>$slug]);
-
-
-        // keep catgory id
-        $categoryId = $category->getId();
-        
-        // display all event form this category
-        $eventsList = $eventRepository->findBy(["category"=>$categoryId ]);
-
-        // error catch events
-        if (!$eventsList)
-        {
-            throw $this->createNotFoundException('No event to display');
-        }
-
-        return $this->render('front/category/index.html.twig', compact('category', 'eventsList'));
+       
+        return $this->render('front/category/index.html.twig', compact('category'));
         
     }
    
