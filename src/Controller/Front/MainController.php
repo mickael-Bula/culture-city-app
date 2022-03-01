03c655ee3b66b84770db81df4058283ee5a6f372
mickael-Bula <bula.mickael@gmail.com>
2022-03-01 09:55:18 +0100

    add/userRepository in MainController::Main

--- src/Controller/Front/MainController.php
+++ src/Controller/Front/MainController.php
@@ -3,8 +3,7 @@
 namespace App\Controller\Front;
 
 
-use App\Repository\{ CategoryRepository, EventRepository };
-use App\Repository\UserRepository;
+use App\Repository\{ CategoryRepository, EventRepository, UserRepository };
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,10 +14,11 @@ class MainController extends AbstractController
      * @Route("/")
      * @Route("/home", name="home")
      */
-    public function index(CategoryRepository $categoryRepository, EventRepository $eventRepository): Response
+    public function index(CategoryRepository $categoryRepository, EventRepository $eventRepository, UserRepository $userRepository): Response
     {
         $categories = $categoryRepository->findAll();
         $events = $eventRepository->findAll();
-        return $this->render('front/main/home.html.twig', compact('events', 'categories'));
+        $users = $userRepository->findAll();
+        return $this->render('front/main/home.html.twig', compact('events', 'categories','users'));
     }
 }
