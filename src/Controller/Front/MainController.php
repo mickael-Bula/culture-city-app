ee36926fcc424d40d714fecae6e64fd22994e8d2
benoit-boulangier <benoit.boulangier@gmail.com>
2022-03-02 13:21:16 +0100

    add/display-events-home-page

--- src/Controller/Front/MainController.php
+++ src/Controller/Front/MainController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller\Front;
 
-
+use App\Entity\Event;
+use App\Entity\User;
 use App\Repository\{ CategoryRepository, EventRepository };
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,7 +19,31 @@ class MainController extends AbstractController
     public function index(CategoryRepository $categoryRepository, EventRepository $eventRepository): Response
     {
         $categories = $categoryRepository->findAll();
+        
+        // On récupère tous les évènements
         $events = $eventRepository->findAll();
-        return $this->render('front/main/home.html.twig', compact('events', 'categories'));
+ 
+        // je récupère la date du jour
+        $currentDate = new DateTime('now');
+        $currentDate = $currentDate->format('Y-m-d');
+
+        // On stocke les évènements dans 2 tableaux
+        $currentEvents = [];
+        $upcomingEvents = [];
+
+        // Formattage des dates pour comparaison de chaque event et stockage des events dans chaque tableau
+        foreach ($events as $event) {            
+            $dateEvent = $event->getStartDate();
+            $dateEvent = $dateEvent->format('Y-m-d');
+            if ($currentDate === $dateEvent)
+            {
+                $currentEvents[] = $event;
+            }
+                $upcomingEvents[] = $event;
+        }
+
+         //dd($currentEvents, $upcomingEvents);
+
+        return $this->render('front/main/home.html.twig', compact('currentEvents', 'upcomingEvents', 'categories'));
     }
 }
