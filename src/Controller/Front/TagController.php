64bf984c028baed0235f162a154c13dadc3a5cf7
benoit-boulangier <benoit.boulangier@gmail.com>
2022-03-03 17:19:53 +0100

    add/tag-display-event-ok

--- src/Controller/Front/TagController.php
+++ src/Controller/Front/TagController.php
@@ -14,9 +14,16 @@ class TagController extends AbstractController
     /**
      * @Route("/tag/{slug}", name="show_tag", methods={"GET"})
      */
-    public function showByName(Tag $tag): Response
+    public function showByName(TagRepository $tagRepository, EventRepository $eventRepository, string $slug): Response
     {
-       
-        return $this->render('front/main/tag.html.twig', compact('tag'));
+        // catch category
+        $tag = $tagRepository->findOneBy(['slug' => $slug]);
+
+        $categorySlug= $tag->getSlug();
+
+        // catch Events form this category orderby date
+        $eventsList = $eventRepository->findByCategory($slug);
+        
+        return $this->render('front/main/tag.html.twig', compact('tag', 'eventsList'));
     }
 }
