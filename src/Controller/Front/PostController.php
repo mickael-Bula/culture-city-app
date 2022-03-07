cdce06d13ec5c11173519858d0698a6a8a040795
SimonChabrier <simonchabrier@yahoo.fr>
2022-03-07 09:28:30 +0100

    debug annotation

--- src/Controller/Front/PostController.php
+++ src/Controller/Front/PostController.php
@@ -10,6 +10,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
