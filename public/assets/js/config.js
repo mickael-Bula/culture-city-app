95f59105640e8590ee6c4aa8160dfb843972d7d6
mickael-Bula <bula.mickael@gmail.com>
2022-03-09 17:31:38 +0100

    expose/config.js for settings

--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -1,5 +1,6 @@
 const config = {
     APIkey: 'ax8dyjCcU47lIHXZfh9JfHM5SCW0USN6',
     LocationKey : 'pk.2da029a812df916f269b26615bf122a6',
+    //! PROD : base_url: 'http://localhost:8000/projet-17-culture-city-app/public/'
     base_url: 'http://localhost:8000/'
 }
