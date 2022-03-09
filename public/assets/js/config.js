125e83b17a49698b8a28d2d7e6812b230646dd54
mickael-Bula <bula.mickael@gmail.com>
2022-03-09 18:21:12 +0100

    add/HTTP verb in apiController annotation

--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -1,6 +1,6 @@
 const config = {
     APIkey: 'ax8dyjCcU47lIHXZfh9JfHM5SCW0USN6',
     LocationKey : 'pk.2da029a812df916f269b26615bf122a6',
-    //! PROD : base_url: 'http://localhost:8000/projet-17-culture-city-app/public/'
+    //! PROD : base_url: 'http://simonchabrier-server.eddi.cloud/projet-17-culture-city-app/public/'
     base_url: 'http://localhost:8000/'
 }
