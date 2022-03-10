d989c1e4162bec4657f18c874a99fecaa25f8786
mickael-Bula <bula.mickael@gmail.com>
2022-03-10 15:55:19 +0100

    add/default position to Agen for demo

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -14,7 +14,6 @@ const app = {
         const userLocation = app.getCoordinates();
 
         // get user's position from cookies if exists and send it to map
-        //! PROD : if (window.location.pathname === '/projet-17-culture-city-app/public/') { mapModule.displayMap(userLocation) }
         if (window.location.pathname === '/') { mapModule.displayMap(userLocation) }
 
         // get locality cookie if exists or set a defaut value if not
@@ -29,7 +28,6 @@ const app = {
     addAllEventListeners: function()
     {
         // we verify if current page is 'home' to handle filters form
-        //!PROD : if (window.location.pathname === '/projet-17-culture-city-app/public/')
         if (window.location.pathname === '/')
         {
             // add listeners to 'categories' buttons
@@ -43,7 +41,6 @@ const app = {
         }
 
         // we verify if current page is 'create/event' to handle endDate's checkbox
-        //! PROD : if (window.location.pathname === '/projet-17-culture-city-app/public/create/event')
         if (window.location.pathname === '/create/event')
         {
             document.getElementById("addEndDate").addEventListener("change", app.handleChangeEventForm);
--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -1,6 +1,5 @@
 const config = {
     APIkey: 'ax8dyjCcU47lIHXZfh9JfHM5SCW0USN6',
     LocationKey : 'pk.2da029a812df916f269b26615bf122a6',
-    //! PROD : base_url: 'http://simonchabrier-server.eddi.cloud/projet-17-culture-city-app/public/'
     base_url: 'http://localhost:8000/'
 }
